349a0a3f3a68a3798a9f3da20eb95ba94b6dfd7d
learner <roadoflearning@live.com>
2017-01-14 19:25:59 +0800

    重写网络请求部分，去除 OkHttpUtils 依赖

--- src/main/java/cn/zheteng123/hdu/network/Network.java
+++ src/main/java/cn/zheteng123/hdu/network/Network.java
@@ -1,16 +1,19 @@
 package cn.zheteng123.hdu.network;
 
 import cn.zheteng123.hdu.pojo.Score;
+import cn.zheteng123.hdu.util.CaptchaUtil;
 import cn.zheteng123.hdu.util.MyCookieJar;
-import com.zhy.http.okhttp.OkHttpUtils;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -20,6 +23,20 @@ import java.util.concurrent.TimeUnit;
  */
 public class Network {
 
+    private static OkHttpClient okHttpClient;
+    private static final String CAPTCHA_PATH = "D:\\Captcha.jpg";
+
+    static {
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        okHttpClient = new OkHttpClient
+                .Builder()
+                .readTimeout(5, TimeUnit.SECONDS)
+                .cookieJar(new MyCookieJar())
+                .addInterceptor(httpLoggingInterceptor)
+                .build();
+    }
+
     /**
      * 根据用户名密码查询成绩信息
      * @param username 用户名（学号）
@@ -28,78 +45,77 @@ public class Network {
      */
     public static List<Score> getScore(String username, String password) throws IOException {
 
-        // 实例化 client，注意实现 cookie 自动管理
-        OkHttpClient client = new OkHttpClient
-                .Builder()
-                .readTimeout(30, TimeUnit.SECONDS)
-                .cookieJar(new MyCookieJar())
-                .build();
-        OkHttpUtils.initClient(client);
-
-
-        // 登录教务系统
-        String dataPostLogin;
 
         // 清除 cookie
-        Response resGetLogout = OkHttpUtils.get()
-                .url("http://jxgl.hdu.edu.cn/logout0.aspx")
-                .addHeader("Referer", "http://jxgl.hdu.edu.cn/xs_main.aspx?xh=" + username)
-                .build()
-                .execute();
-        resGetLogout.close();
-
-        Response resGetLogout2 = OkHttpUtils.get()
-                .url("http://i.hdu.edu.cn/dcp/logout0.jsp")
-                .addHeader("Referer", "http://jxgl.hdu.edu.cn/logout0.aspx")
-                .build()
-                .execute();
-        resGetLogout2.close();
-
-
+        ((MyCookieJar)okHttpClient.cookieJar()).deleteCookie();
 
         // Get 访问登录页，获取隐藏表单项 lt
-        Response resGetLt = OkHttpUtils.get()
+        Request reqGetLt = new Request.Builder().get()
                 .url("http://jxgl.hdu.edu.cn/")
-                .build()
-                .execute();
+                .build();
+        Response resGetLt = okHttpClient.newCall(reqGetLt).execute();
         String dataLt = resGetLt.body().string();
         Document docLt = Jsoup.parse(dataLt);
         String lt = docLt.select("input[name=lt]").val();
         System.out.println(lt);
         resGetLt.close();
 
+        // 识别验证码
+        Request reqGetCaptcha = new Request.Builder().get()
+                .url("http://cas.hdu.edu.cn/cas/Captcha.jpg")
+                .build();
+        Response resGetCaptcha = okHttpClient.newCall(reqGetCaptcha).execute();
+        InputStream isCaptcha = resGetCaptcha.body().byteStream();
+        File file = new File(CAPTCHA_PATH);
+        OutputStream os = new FileOutputStream(file);
+        int bytesRead = 0;
+        byte[] buffer = new byte[8192];
+        while ((bytesRead = isCaptcha.read(buffer, 0, 8192)) != -1) {
+            os.write(buffer, 0, bytesRead);
+        }
+        os.close();
+        isCaptcha.close();
+        String captchaResult = CaptchaUtil.recogniseCaptcha(CAPTCHA_PATH);
+        System.out.println(captchaResult);
 
         // Post 登录教务系统
-        Response resPostLogin = OkHttpUtils.post()
+        Request reqPostLogin = new Request.Builder().post(
+                new FormBody.Builder()
+                        .add("username", username)
+                        .add("password", password)
+                        .add("lt", lt)
+                        .add("captcha", captchaResult)
+                        .build())
                 .url("http://cas.hdu.edu.cn/cas/login")
-                .addParams("username", username)
-                .addParams("password", password)
-                .addParams("lt", lt)
-                .build()
-                .execute();
-        dataPostLogin = resPostLogin.body().string();
+                .build();
+        Response resPostLogin = okHttpClient.newCall(reqPostLogin).execute();
+        String dataPostLogin = resPostLogin.body().string();
         if (dataPostLogin.contains("账号或密码错误")) {
             System.out.println("账号或密码错误！");
+            return null;
+        }
+        if (dataPostLogin.contains("错误信息提示")) {
+            System.out.println("验证码错误！");
             return null;
         }
         resPostLogin.close();
 
 
         // 进入选课系统
-        Response resGetEduAdmin = OkHttpUtils.get()
+        Request reqGetEduAdmin = new Request.Builder().get()
                 .url("http://i.hdu.edu.cn/dcp/forward.action?path=dcp/apps/sso/jsp/ssoDcpSelf&appid=1142")
-                .build()
-                .execute();
+                .build();
+        Response resGetEduAdmin = okHttpClient.newCall(reqGetEduAdmin).execute();
         String dataTemp = resGetEduAdmin.body().string();
         resGetEduAdmin.close();
 //        System.out.println(dataTemp);
         while (dataTemp.contains("认证转向")) {
             Document docUrlJump = Jsoup.parse(dataTemp);
             String urlJump = docUrlJump.select("a").get(0).attr("href");
-            Response resTemp = OkHttpUtils.get()
+            Request reqTemp = new Request.Builder().get()
                     .url(urlJump)
-                    .build()
-                    .execute();
+                    .build();
+            Response resTemp = okHttpClient.newCall(reqTemp).execute();
             dataTemp = resTemp.body().string();
             resTemp.close();
         }
@@ -108,17 +124,19 @@ public class Network {
 
         // 获取成绩
         System.out.println("获取成绩");
-        Response resGetScore = OkHttpUtils.get()
+        Request reqGetScore = new Request.Builder().get()
                 .url("http://jxgl.hdu.edu.cn/xscjcx_dq.aspx?xh=" + username)
                 .addHeader("Referer", "http://jxgl.hdu.edu.cn/xscjcx_dq.aspx?xh=" + username)
-                .build()
-                .execute();
+                .build();
+        Response resGetScore = okHttpClient.newCall(reqGetScore).execute();
         String dataScore = resGetScore.body().string();
         resGetScore.close();
         Document docScore = Jsoup.parse(dataScore);
         Elements trs = docScore.select("#DataGrid1 tr");
 //        System.out.println(trs.size());
-        trs.remove(0);  // 删去表格的第一行表头
+        if (trs.size() > 0) {
+            trs.remove(0);  // 删去表格的第一行表头
+        }
         List<Score> scoreList = new ArrayList<Score>();
         for (Element tr : trs) {
             Elements tds = tr.select("td");
